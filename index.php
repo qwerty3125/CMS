1fa4943b309b77657174fd91d742e868ea142b93
Danil Nagovitsyn <Windeta312@yandex.ru>
2020-10-09 12:36:38 +0300

    main

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CMS</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container h-100 main">
+            <div class="row main-form">
+                <form class="" method="post" action="#">
+                    <div class="form-group">
+                        <div class="title">Authorization</div>
+                    </div>    
+
+                    <div class="form-group">
+                        <label for="username" class="cols-sm-2 control-label">Login</label>
+                        <div class="cols-sm-10">
+                        <div class="input-group">
+                        <span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i></span>
+                        <input type="text" class="form-control" name="username" id="username" placeholder="Enter your Username"/>
+                        </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password" class="cols-sm-2 control-label">Password</label>
+                        <div class="cols-sm-10">
+                        <div class="input-group">
+                        <span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+                        <input type="password" class="form-control" name="password" id="password" placeholder="Enter your Password"/>
+                        </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group ">
+                        <a href="#" target="_blank" type="button" id="button" class="btn btn-primary btn-lg btn-block login-button">Enter</a>
+                    </div>
+                
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
